694b347d449cc6cbbf335876f20855ad745181da
Stephen Done <srdone@gmail.com>
2015-05-14 06:19:42 -0600

    added specs, need to flesh out with tests

--- test/TableSpec.jsx
+++ test/TableSpec.jsx
@@ -9,4 +9,40 @@ describe('Table', () => {
     TestUtils.renderIntoDocument(<Table />);
   });
 
+  it('should render the header prop values as table headers', () => {
+
+  });
+
+  it('should render a caption, if specified as a caption prop', () => {
+
+  });
+
+  it('should render the specified number of columns', () => {
+
+  });
+
+  it('should render the children into the cells', () => {
+
+  });
+
+  it('should render enough rows to use all the children as table cells', () => {
+
+  });
+
+  it('should render the specified number of rows, if specified', () => {
+
+  });
+
+  it('should log a warning if there are more children than can fit', () => {
+
+  });
+
+  it('should log a warning if there not enough children to fill all the rows', () => {
+
+  });
+
+  it('should allow for sorting', () => {
+
+  });
+
 });
