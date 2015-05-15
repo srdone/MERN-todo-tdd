539eb79dbc5cd718728cec13bd975cd2f70b585a
Stephen Done <srdone@gmail.com>
2015-05-14 19:09:43 -0600

    renders header and caption

--- public/components/Table.jsx
+++ public/components/Table.jsx
@@ -2,8 +2,32 @@ var React = require('react');
 
 export default class Table extends React.Component {
 
+  static generateCaption(captionItem) {
+    if (captionItem) {
+      return <caption>{captionItem}</caption>
+    }
+  }
+
+  static generateHead(numberOfColumns) {
+    var cells = [];
+    for (var i = 0; i < numberOfColumns; i++) {
+      cells.push(<th key={i}></th>);
+    };
+
+    return <thead><tr>{cells}</tr></thead>
+  }
+
   render() {
-    return <div></div>
+
+    var caption = Table.generateCaption(this.props.caption);
+
+    var head = Table.generateHead(this.props.columns);
+
+    return <table>
+      {caption}
+      {head}
+      <tbody></tbody>
+    </table>
   }
 
 }
--- test/TableSpec.jsx
+++ test/TableSpec.jsx
@@ -5,19 +5,42 @@ var Table = require('../public/components/Table');
 
 describe('Table', () => {
 
+  var element, elementDOMNode, numberOfColumns;
+
+  beforeEach(() => {
+    spyOn(Table, 'generateHead').and.callThrough();
+    numberOfColumns = 3;
+    element = TestUtils.renderIntoDocument(<Table columns={numberOfColumns} />);
+    elementDOMNode = React.findDOMNode(element);
+  })
+
   it('should render', () => {
-    TestUtils.renderIntoDocument(<Table />);
+    expect(element).toBeTruthy();
   });
 
-  it('should render the header prop values as table headers', () => {
-
+  it('should render the outer dom node as table', () => {
+    expect(elementDOMNode.tagName.toLowerCase()).toBe('table');
   });
 
-  it('should render a caption, if specified as a caption prop', () => {
-
+  it('should render the first inside dom node as thead', () => {
+    expect(elementDOMNode.children[0].tagName.toLowerCase()).toBe('thead');
   });
 
-  it('should render the specified number of columns', () => {
+  it('should render the second inside dom node as tbody', () => {
+    expect(elementDOMNode.children[1].tagName.toLowerCase()).toBe('tbody');
+  });
+
+  it('should call the Table.generateHead method to be called with the number of children', () => {
+    expect(Table.generateHead).toHaveBeenCalledWith(numberOfColumns);
+  });
+
+  it('should render the specified number of columns in the head', () => {
+    var head = elementDOMNode.children[0];
+
+    expect(head.children[0].children.length).toBe(numberOfColumns);
+  });
+
+  it('should render the specified number of columns in the body', () => {
 
   });
 
@@ -44,5 +67,36 @@ describe('Table', () => {
   it('should allow for sorting', () => {
 
   });
+
+});
+
+describe('Table with caption', () => {
+
+  var caption, renderedCaption, elementWithCaption, elementWithCaptionDOMNode;
+
+  beforeEach(() => {
+    //we have to set the spy before we call the function in beforeEach
+    //if we want the function to still call, we have to call .and.callThrough();
+    spyOn(Table, 'generateCaption').and.callThrough();
+    caption = 'Caption';
+    elementWithCaption = TestUtils.renderIntoDocument(<Table caption={caption} />);
+    elementWithCaptionDOMNode = React.findDOMNode(elementWithCaption);
+  });
+
+  it('should render a caption, if specified as a caption prop', () => {
+    expect(elementWithCaptionDOMNode.children[0].tagName.toLowerCase()).toBe('caption');
+  });
+
+  it('should render the text contents of the passed caption object as the innerHTML of the caption', () =>{
+    expect(elementWithCaptionDOMNode.children[0].innerHTML).toBe(caption);
+  });
+
+  it('should call the Table.generateCaption function', () => {
+    expect(Table.generateCaption).toHaveBeenCalled();
+  });
+
+  it('should call the Table.generateCaption function with the caption that was passed in', () => {
+    expect(Table.generateCaption).toHaveBeenCalledWith(caption);
+  })
 
 });
